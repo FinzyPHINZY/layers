c9e7b294a9d3e3fb8284bb2187be637ff29b7787
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-09 15:29:13 +0100

    feat: implement responsive nav menu

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import logo from '@/assets/images/logo.svg';
+import Button from '@/components/Button';
 
 const navLinks = [
   { label: 'Home', href: '#' },
@@ -10,13 +11,22 @@ const navLinks = [
 
 export default function Navbar() {
   return (
-    <section className="py-4">
-      <div className="container">
-        <div className="grid grid-cols-2 items-center border border-white/15 rounded-full p-2 px-4">
+    <section className="py-4 lg:py-8">
+      <div className="container max-w-5xl">
+        <div className="grid grid-cols-2 lg:grid-cols-3 items-center border border-white/15 rounded-full p-2 px-4 md:pr-2">
           <div className="">
-            <Image src={logo} alt="Logo" className="w-auto h-9" />
+            <Image src={logo} alt="Logo" className="w-auto h-9 md:h-auto" />
           </div>
-          <div className="flex border justify-end">
+          <div className="lg:flex justify-center items-center hidden">
+            <nav className="flex gap-6 font-medium">
+              {navLinks.map((link) => (
+                <a href="link.href" key={link.label}>
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+          <div className="flex justify-end gap-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -24,15 +34,27 @@ export default function Navbar() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="feather feather-menu"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-menu md:hidden"
             >
               <line x1="3" y1="12" x2="21" y2="12"></line>
               <line x1="3" y1="6" x2="21" y2="6"></line>
               <line x1="3" y1="18" x2="21" y2="18"></line>
             </svg>
+            <Button
+              variant="secondary"
+              className="hidden md:inline-flex items-center"
+            >
+              Log In
+            </Button>
+            <Button
+              variant="primary"
+              className="hidden md:inline-flex items-center"
+            >
+              Sign Up
+            </Button>
           </div>
         </div>
       </div>
