d4413bda7f45e1f4191ab0ee4888342653e86c2e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-11 15:10:26 +0100

    feat: implment responsive footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,31 @@
+import logoImage from '@/assets/images/logo.svg';
+import Image from 'next/image';
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+  { href: '#', label: 'Contact' },
+  { href: '#', label: 'Privacy Policy' },
+  { href: '#', label: 'Terms & Conditions' },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+  return (
+    <section className="py-16">
+      <div className="container">
+        <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+          <div className="">
+            <Image src={logoImage} alt="logo" className="" />
+          </div>
+          <div className="">
+            <nav className="flex gap-6">
+              {footerLinks.map((link, i) => (
+                <a href={link.href} className="text-white/50 text-sm" key={i}>
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
