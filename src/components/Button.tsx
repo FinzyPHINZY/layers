22285d573abd0b434b84cee7ba4139107362f579
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-11 04:46:27 +0100

    feat: implement hero section with design images

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
-import { HTMLAttributes } from 'react';
+import { ButtonHTMLAttributes } from 'react';
 import { cva } from 'class-variance-authority';
 
 const classes = cva('border h-12 rounded-full px-6 font-medium', {
   variants: {
     variant: {
       primary: 'bg-lime-400 text-neutral-950 border-lime-400',
-      secondary: ' border-white text-white bg-transparent',
+      secondary: 'border-white text-white bg-transparent',
+    },
+    size: {
+      sm: 'h-10',
     },
   },
 });
@@ -13,14 +16,13 @@ const classes = cva('border h-12 rounded-full px-6 font-medium', {
 export default function Button({
   className,
   variant,
+  size,
   ...otherProps
 }: {
   variant: 'primary' | 'secondary';
-} & HTMLAttributes<HTMLButtonElement>) {
+  size?: 'sm';
+} & ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button
-      className={classes({ variant: variant, className })}
-      {...otherProps}
-    />
+    <button className={classes({ variant, size, className })} {...otherProps} />
   );
 }
